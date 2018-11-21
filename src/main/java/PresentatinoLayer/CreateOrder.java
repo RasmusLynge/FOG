d1a4a04e36a3ee5b79990ac13bddf7dc22d8c798
Magnus <magnus.akm@gmail.com>
2018-11-21 13:01:34 +0100

    hard hotfix

---
+++ src/main/java/PresentatinoLayer/CreateOrder.java
@@ -0,0 +1,38 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PresentatinoLayer;
+
+import FunctionLayer.GeneralException;
+import FunctionLayer.LogicFacade;
+import FunctionLayer.Order;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Rasmu
+ */
+public class CreateOrder extends Command {
+
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws GeneralException {
+        LogicFacade lf = new LogicFacade();
+        String width = request.getParameter("widthnumber");
+        String length = request.getParameter("lengthnumber");
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String zip = request.getParameter("zip");
+        String phone = request.getParameter("phone");
+        String evt = request.getParameter("evt");
+        
+        Order o = lf.makeOrder(Integer.parseInt(width), Integer.parseInt(length), name, email, zip, phone, evt);
+        request.getSession().setAttribute("order", o);
+       return "singleOrder";
+    }
+    
+}
--- src/main/java/PresentatinoLayer/GetOrderDetails.java
+++ src/main/java/PresentatinoLayer/GetOrderDetails.java
@@ -3,21 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package PresentatinoLayer;
 
 import FunctionLayer.GeneralException;
+import FunctionLayer.LogicFacade;
+import FunctionLayer.Order;
+import FunctionLayer.User;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author Rasmu
+ * @author Simon Bojesen
  */
-public class OrderPage extends Command {
-
+public class GetOrderDetails extends Command {
+    
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws GeneralException {
-
-        return "orderpage";
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        
+        return "showorderdetails";
     }
 }
