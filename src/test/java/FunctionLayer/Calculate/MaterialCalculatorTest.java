a797d810ccad2661a81342695aca7048e500c1ad
Simon Bojesen <simon@schonberg.dk>
2018-12-07 13:33:18 +0100

    grænseværdier

--- src/test/java/FunctionLayer/Calculate/MaterialCalculatorTest.java
+++ src/test/java/FunctionLayer/Calculate/MaterialCalculatorTest.java
@@ -9,6 +9,7 @@ import FunctionLayer.Entity.Carport;
 import FunctionLayer.Entity.Material;
 import FunctionLayer.Exception.GeneralException;
 import FunctionLayer.Exception.MakeOrderException;
+import static FunctionLayer.Rule.Rules.PLANKLENGTH;
 import static FunctionLayer.Rule.Rules.PLASTMOLENGTHLONG;
 import static FunctionLayer.Rule.Rules.PLASTMOLENGTHSMALL;
 import java.util.ArrayList;
@@ -38,7 +39,7 @@ public class MaterialCalculatorTest {
     boolean shed = false;
 
     @Test
-    public void testPlastmoLongAmount() throws GeneralException, MakeOrderException {
+    public void testPlastmoLongAmountFlatRoof() throws GeneralException, MakeOrderException {
         //act
         Carport result = cc.calculateAll(length, width, roof, shed);
         result.setList(mc.materialList(result));
@@ -46,45 +47,63 @@ public class MaterialCalculatorTest {
         //Assert
         for (int i = 0; i < l.size(); i++) {
             if (l.get(i).getName().equals("Plastmo Ecolite blåtonet") && l.get(i).getLength() == PLASTMOLENGTHLONG) {
+                //bruges kun til fladt tag som den er her derfor 4
                 int expectedPlastmoLongAmount = 4;
                 int actualPlastmoLongAmount = l.get(i).getAmount();
+
                 Assert.assertEquals(expectedPlastmoLongAmount, actualPlastmoLongAmount);
             }
         }
     }
     
     @Test
-    public void testPlastmoSmallAmount() throws GeneralException, MakeOrderException {
-        //act
-        Carport result = cc.calculateAll(length, width, roof, shed);
+    public void testPlastmoLongAmountTopRoof() throws GeneralException, MakeOrderException {
+        Carport result = cc.calculateAll(length, width, true, shed);
         result.setList(mc.materialList(result));
         ArrayList<Material> l = result.getList();
-        //Assert
         for (int i = 0; i < l.size(); i++) {
-            if (l.get(i).getName().equals("Plastmo Ecolite blåtonet") && l.get(i).getLength() == PLASTMOLENGTHSMALL) {
-                int expectedPlastmoSmallAmount = 0;
-                int actualPlastmoSmallAmount = l.get(i).getAmount();
-                Assert.assertEquals(expectedPlastmoSmallAmount, actualPlastmoSmallAmount);
+            if (l.get(i).getName().equals("Plastmo Ecolite blåtonet") && l.get(i).getLength() == PLASTMOLENGTHLONG) {
+                //bruges kun til fladt tag, så bør være 0
+                int expectedPlastmoLongAmount = 0;
+                int actualPlastmoLongAmount = l.get(i).getAmount();
+
+                Assert.assertEquals(expectedPlastmoLongAmount, actualPlastmoLongAmount);
             }
         }
     }
 
     @Test
-    public void testPostAmount() throws GeneralException, MakeOrderException {
-        //act
+    public void testPlastmoSmallAmountFlatRoof() throws GeneralException, MakeOrderException {
         Carport result = cc.calculateAll(length, width, roof, shed);
         result.setList(mc.materialList(result));
         ArrayList<Material> l = result.getList();
-        //Assert
         for (int i = 0; i < l.size(); i++) {
-            if (l.get(i).getName().equals("97x97	mm.	trykimp. Stolpe")) {
-                int expectedPostAmount = 4;
-                int actualPostAmount = l.get(i).getAmount();
-                Assert.assertEquals(expectedPostAmount, actualPostAmount);
+            if (l.get(i).getName().equals("Plastmo Ecolite blåtonet") && l.get(i).getLength() == PLASTMOLENGTHSMALL) {
+                //bruges kun til fladt tag, så bør være 0
+                int expectedPlastmoSmallAmount = 0;
+                int actualPlastmoSmallAmount = l.get(i).getAmount();
+
+                Assert.assertEquals(expectedPlastmoSmallAmount, actualPlastmoSmallAmount);
             }
         }
     }
     
+    @Test
+    public void testPlastmoSmallAmountTopRoof() throws GeneralException, MakeOrderException {
+        Carport result = cc.calculateAll(length, width, true, shed);
+        result.setList(mc.materialList(result));
+        ArrayList<Material> l = result.getList();
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getName().equals("Plastmo Ecolite blåtonet") && l.get(i).getLength() == PLASTMOLENGTHSMALL) {
+                //bruges kun til fladt tag, så bør være 0
+                int expectedPlastmoLongAmount = 0;
+                int actualPlastmoLongAmount = l.get(i).getAmount();
+
+                Assert.assertEquals(expectedPlastmoLongAmount, actualPlastmoLongAmount);
+            }
+        }
+    }
+
     @Test
     public void testScrewBoxesAmount() throws GeneralException, MakeOrderException {
         //act
@@ -97,6 +116,134 @@ public class MaterialCalculatorTest {
                 int expectedScrewBoxesAmount = 2;
                 int actualScrewBoxesAmount = l.get(i).getAmount();
                 Assert.assertEquals(expectedScrewBoxesAmount, actualScrewBoxesAmount);
+            }
+        }
+    }
+
+    @Test
+    public void testFlatHingesAmount() throws GeneralException, MakeOrderException {
+        //act
+        Carport result = cc.calculateAll(length, width, roof, shed);
+        result.setList(mc.materialList(result));
+        ArrayList<Material> l = result.getList();
+        //Assert
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getName().equals("FladtBeslag")) {
+                int expectedFlatHingesAmount = 2;
+                int actualFlatHingesAmount = l.get(i).getAmount();
+                Assert.assertEquals(expectedFlatHingesAmount, actualFlatHingesAmount);
+            }
+        }
+    }
+
+    @Test
+    public void testRoofTilesAmount() throws GeneralException, MakeOrderException {
+        //act
+        Carport result = cc.calculateAll(length, width, roof, shed);
+        result.setList(mc.materialList(result));
+        ArrayList<Material> l = result.getList();
+        //Assert
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getName().equals("Tegl")) {
+                int expectedRoofTilesAmount = 2;
+                int actualRoofTilesAmount = l.get(i).getAmount();
+                Assert.assertEquals(expectedRoofTilesAmount, actualRoofTilesAmount);
+            }
+        }
+    }
+
+    @Test
+    public void testLHingesAmount() throws GeneralException, MakeOrderException {
+        //act
+        Carport result = cc.calculateAll(length, width, roof, shed);
+        result.setList(mc.materialList(result));
+        ArrayList<Material> l = result.getList();
+        //Assert
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getName().equals("LBeslag")) {
+                int expectedLHingesAmount = 32;
+                int actualLHingesAmount = l.get(i).getAmount();
+                Assert.assertEquals(expectedLHingesAmount, actualLHingesAmount);
+            }
+        }
+    }
+
+    @Test
+    public void testPlanksAmount() throws GeneralException, MakeOrderException {
+        //act
+        Carport result = cc.calculateAll(length, width, roof, shed);
+        result.setList(mc.materialList(result));
+        ArrayList<Material> l = result.getList();
+        //Assert
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getName().equals("25x150	mm.	trykimp. Bræt") && l.get(i).getLength() == PLANKLENGTH) {
+                int expectedPlanksAmount = 2;
+                int actualPlanksAmount = l.get(i).getAmount();
+                Assert.assertEquals(expectedPlanksAmount, actualPlanksAmount);
+            }
+        }
+    }
+
+    @Test
+    public void testCoverStabilizerPlankSmallAmount() throws GeneralException, MakeOrderException {
+        //act
+        Carport result = cc.calculateAll(length, width, roof, shed);
+        result.setList(mc.materialList(result));
+        ArrayList<Material> l = result.getList();
+        //Assert
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getName().equals("45x95 Reglar ubh.") && l.get(i).getLength() == 240) {
+                int expectedCoverStabilizerPlankSmallAmount = 2;
+                int actualCoverStabilizerPlankSmallAmount = l.get(i).getAmount();
+                Assert.assertEquals(expectedCoverStabilizerPlankSmallAmount, actualCoverStabilizerPlankSmallAmount);
+            }
+        }
+    }
+
+    @Test
+    public void testCoverStabilizerPlanksLongAmount() throws GeneralException, MakeOrderException {
+        //act
+        Carport result = cc.calculateAll(length, width, roof, shed);
+        result.setList(mc.materialList(result));
+        ArrayList<Material> l = result.getList();
+        //Assert
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getName().equals("45x95 Reglar ubh.") && l.get(i).getLength() == 360) {
+                int expectedCoverStabilizerPlanksLongAmount = 2;
+                int actualCoverStabilizerPlanksLongAmount = l.get(i).getAmount();
+                Assert.assertEquals(expectedCoverStabilizerPlanksLongAmount, actualCoverStabilizerPlanksLongAmount);
+            }
+        }
+    }
+
+    @Test
+    public void testDoorKnobAmount() throws GeneralException, MakeOrderException {
+        //act
+        Carport result = cc.calculateAll(length, width, roof, shed);
+        result.setList(mc.materialList(result));
+        ArrayList<Material> l = result.getList();
+        //Assert
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getName().equals("Dørhåndtag")) {
+                int expectedDoorKnobAmount = 2;
+                int actualDoorKnobAmount = l.get(i).getAmount();
+                Assert.assertEquals(expectedDoorKnobAmount, actualDoorKnobAmount);
+            }
+        }
+    }
+
+    @Test
+    public void testDoorHingeAmount() throws GeneralException, MakeOrderException {
+        //act
+        Carport result = cc.calculateAll(length, width, roof, shed);
+        result.setList(mc.materialList(result));
+        ArrayList<Material> l = result.getList();
+        //Assert
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getName().equals("Dør hængsel")) {
+                int expectedDoorHingeAmount = 0;
+                int actualDoorHingeAmount = l.get(i).getAmount();
+                Assert.assertEquals(expectedDoorHingeAmount, actualDoorHingeAmount);
             }
         }
     }
