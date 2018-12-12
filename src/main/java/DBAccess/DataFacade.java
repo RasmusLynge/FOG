5e5dee02545ab69cd18059cf0ab52d2e4cde073f
Mathias <Mathias@10.50.138.118>
2018-12-12 15:44:50 +0100

    javadoc 3

--- src/main/java/DBAccess/DataFacade.java
+++ src/main/java/DBAccess/DataFacade.java
@@ -16,9 +16,9 @@ public class DataFacade {
 
     /**
      * This method calls the corresponding DataMapper method and returns the return values. 
-     * @param state
-     * @return
-     * @throws DMException
+     * @param state the state of the order
+     * @return Returns an arraylist of orders with the corresponding state
+     * @throws DMException if the query fails
      */
     public ArrayList<Order> getSpecificOrders(String state) throws DMException {
         return dm.getSpecificOrders(state);
@@ -26,9 +26,9 @@ public class DataFacade {
 
     /**
      * This method calls the corresponding DataMapper method and returns the return values.
-     * @param email
-     * @param password
-     * @return
+     * @param email email of the user
+     * @param password password of the user
+     * @return returns the user if found
      * @throws LoginException
      */
     public User login(String email, String password) throws LoginException {
@@ -37,7 +37,8 @@ public class DataFacade {
 
     /**
      * This method calls the corresponding DataMapper method and returns the return values. 
-     * @return @throws DMException
+     * @return ArrayList of orders from the query
+     * @throws DMException if the query fails
      */
     public ArrayList<Order> getAllOrders() throws DMException {
         return dm.getAllOrders();
@@ -45,20 +46,20 @@ public class DataFacade {
 
     /**
      * This method calls the corresponding DataMapper method.
-     * @param o
-     * @throws DMException
-     * @throws MakeOrderException
+     * @param order the order that is to be inserted in the database
+     * @throws DMException if the query fails
+     * @throws MakeOrderException if it fails to make the order
      */
-    public void makeOrder(Order o) throws DMException, MakeOrderException {
-        dm.createOrder(o);
+    public void makeOrder(Order order) throws DMException, MakeOrderException {
+        dm.createOrder(order);
     }
 
     /**
      * This method calls the corresponding DataMapper method and returns the return values. 
-     * @param orderid
-     * @return
-     * @throws DMException
-     * @throws MakeOrderException
+     * @param orderid the ID of the order
+     * @return Returns the order with the specified ID
+     * @throws DMException if the query fails
+     * @throws MakeOrderException if it fails to make the order
      */
     public Order getOrderByID(int orderid) throws DMException, MakeOrderException {
         return dm.getOrderByID(orderid);
@@ -67,14 +68,14 @@ public class DataFacade {
 
     /**
      * This method calls the corresponding DataMapper method and returns the return values. 
-     * @param orderId
-     * @param desiredWidth
-     * @param desiredLength
-     * @param flatRoof
-     * @param state
-     * @return
-     * @throws DMException
-     * @throws MakeOrderException
+     * @param orderId the ID of the order
+     * @param desiredWidth the new width of the order
+     * @param desiredLength the new length of the order
+     * @param flatRoof the new boolean value for roof 
+     * @param state the new String state
+     * @return returns the order after it has been updated in the database
+     * @throws DMException if the query fails
+     * @throws MakeOrderException if it fails to make the order
      */
     public Order EditOrder(int orderId, int desiredWidth, int desiredLength, int flatRoof, String state) throws DMException, MakeOrderException {
         Order o = dm.EditOrder(orderId, desiredLength, desiredWidth, flatRoof, state);
@@ -83,7 +84,8 @@ public class DataFacade {
 
     /**
      * This method calls the corresponding DataMapper method and returns the return values. 
-     * @return @throws DMException
+     * @return returns an ArrayList of Materials
+     * @throws DMException if the query fails
      */
     public ArrayList<Material> getMaterials() throws DMException {
         ArrayList<Material> list = dm.getMaterials();
